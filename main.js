0f0892a921dae5075c4ce3f01f57e7fc7281d4a0
toggi0991 <toggi0991@naver.com>
2022-02-23 01:11:24 +0900

    complete

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-function
+function x_y(){
+    var x = document.getElementById("x").value;
+    var y = document.getElementById("y").value;
+
+    if(x == 0 && y == 0){
+        document.getElementById("result_1").value = "";
+    }else{
+        document.getElementById("result_1").value = x+(100-x)*(y/100);
+    }
+}
+
+
+function a_b(){
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+
+    
+
+    if(a == 0 && b == 0){
+        document.getElementById("result_2").value = "";
+    }else{
+        document.getElementById("result_2").value = b*a-a+100;
+    }
+}
