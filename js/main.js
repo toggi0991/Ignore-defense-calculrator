9c7c74f4e6f5396c9318723e8ebaba009b6146d7
toggi0991 <toggi0991@naver.com>
2022-02-23 01:58:00 +0900

    아 편안~

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ function x_y(){
     var x = document.getElementById("x").value;
     var y = document.getElementById("y").value;
 
-    if(x == 0 && y == 0){
+    if(x == 0 || y == 0){
         document.getElementById("result_1").value = "";
     }else{
         document.getElementById("result_1").value = x+(100-x)*(y/100);
@@ -16,7 +16,7 @@ function a_b(){
 
     
 
-    if(a == 0 && b == 0){
+    if(a == 0 || b == 0){
         document.getElementById("result_2").value = "";
     }else{
         document.getElementById("result_2").value = b*a-a+100;
